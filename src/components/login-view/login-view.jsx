5c5bd84e6ff68443d099d39d111cafb6a798229c
mind28 <96925869+mind28@users.noreply.github.com>
2022-03-15 17:33:49 -0400

    login and registration views

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+  };
+
+  return (
+      <div>
+          <div className='login'>
+          <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+            
+            </form>
+
+        </div>
+        <button onClick={() => {alert('Nice!')}}>Register Now</button>
+      </div>
+
+      
+      
+    
+    
+
+    
+    
+  );
+}
